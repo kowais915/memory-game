b5b21150c7c8bfce2cd756e4ef52f89b3ffa6151
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-19 03:27:27 +0500

    :construction: added a function to shuffle cards, added a state to keep track of turns

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
+import {useState } from 'react';
 
+const images = [
+  {"src": "/img/clock.jpg"},
+  {"src": "/img/dome.jpg"},
+  {"src": "/img/fcse.jpg"},
+  {"src": "/img/gate.jpg"},
+  {"src": "/img/lib.jpg"},
+  {"src": "/img/tuck.jpg"},
+]
 
 function App() {
+const [card, setCard ] = useState([])
+const[turn, setTurn ] = useState(0)
+
+
+  const shuffle = () =>{
+    const shuffledCards = [...images, ...images]
+      .sort(()=>{
+        return (Math.random() - 0.5)
+      })
+      .map((card ) => ({...card, id: Math.random()}))
+
+      setCard(shuffledCards)
+      setTurn(0)
+  }
+
+  console.log(card, turn)
   return (
     <div className="App">
       <div className="nav">
           <h1 className="title">Memory Game!</h1>
-          <button className="btn">Reset Game</button>
+          <button className="btn" onClick={shuffle}>Reset Game</button>
       </div>
       
 
