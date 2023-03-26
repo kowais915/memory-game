f529dbdb1c865ee969355f4ea153ab5c16a564b5
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-26 16:08:47 +0500

    :lipstick: styled the navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,17 @@
+import '../styles/Navbar.css'
+
 const Navbar = ({shuffle}) => {
     return (
             <div className="nav">
                 <h1 className="title">Memory Game!</h1>
-                <div>
+                <div className='links'>
                 <ul>
                     <li><a href="">Code</a></li>
                     <li><a href="">About</a></li>
                     <li><a href="">Portfolio</a></li>
                 </ul>
                 <button className="btn" onClick={shuffle}>New Game</button>
-                
+
                 </div>
              </div>
       );
