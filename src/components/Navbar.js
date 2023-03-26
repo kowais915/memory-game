ee901d68802e849b4d64a2c5abcd398f4094997f
Owais Khan <45107209+kowais915@users.noreply.github.com>
2023-03-26 16:13:17 +0500

    :sparkles: done with the navbar for now

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,15 +4,19 @@ const Navbar = ({shuffle}) => {
     return (
             <div className="nav">
                 <h1 className="title">Memory Game!</h1>
+
                 <div className='links'>
-                <ul>
-                    <li><a href="">Code</a></li>
-                    <li><a href="">About</a></li>
-                    <li><a href="">Portfolio</a></li>
-                </ul>
-                <button className="btn" onClick={shuffle}>New Game</button>
+                    <ul>
+                        <li><a href="">Code</a></li>
+                        <li><a href="">About</a></li>
+                        <li><a href="">Portfolio</a></li>
+                    </ul>
+                   
 
                 </div>
+
+                <button className="btn" onClick={shuffle}>New Game</button>
+
              </div>
       );
 }
